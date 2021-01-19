3848c6e694140cd0eaca9f1f32e96d8c8e07776c
La Quốc Thắng <quocthang0507@gmail.com>
2021-01-19 19:26:42 +0700

    Add main function to project

--- MainActivity.cs
+++ MainActivity.cs
@@ -3,24 +3,56 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System;
+using QRScanner.Services;
+using ZXing.Mobile;
 
 namespace QRScanner
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity
+	{
+		private Button btnClick;
+		private TextView txtBarcode;
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.activity_main);
+
+			btnClick = FindViewById<Button>(Resource.Id.btnScan);
+			txtBarcode = FindViewById<TextView>(Resource.Id.txtBarcode);
+
+			btnClick.Click += btnScan_Clicked;
+
+			MobileBarcodeScanner.Initialize(Application);
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+		{
+			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+
+		private async void btnScan_Clicked(object sender, EventArgs e)
+		{
+			try
+			{
+				var scanner = new QRScanningService();
+				var result = await scanner.ScanAsync();
+				if (result != null)
+				{
+					txtBarcode.Text = result;
+				}
+			}
+			catch (Exception ex)
+			{
+
+				throw;
+			}
+		}
+	}
 }
---
+++ Services/QRScanningService.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing.Mobile;
+
+namespace QRScanner.Services
+{
+	public class QRScanningService : IQRScanningService
+	{
+		public async Task<string> ScanAsync()
+		{
+			var optionsDefault = new MobileBarcodeScanningOptions();
+			var optionsCustom = new MobileBarcodeScanningOptions();
+			var scanner = new MobileBarcodeScanner()
+			{
+				TopText = "Scan the QR Code",
+				BottomText = "Please wait...",
+				CancelButtonText = "Cancel"
+			};
+			var scanResult = await scanner.Scan(optionsCustom);
+			return scanResult.Text;
+		}
+	}
+}
